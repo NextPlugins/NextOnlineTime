0eb72dc496ef74ac2f5d11c51042a8d7c402596c
Yuhtin <rio.daviduarte@gmail.com>
2020-12-27 19:03:41 -0300

    feat(sql): sql connection
    Added sql w/ provider of @HenryFabio
    
    Co-Authored-By: Henry Fábio <35360090+HenryFabio@users.noreply.github.com>

--- src/main/java/com/yuhtin/minecraft/lyces/tempoonline/TempoOnline.java
+++ src/main/java/com/yuhtin/minecraft/lyces/tempoonline/TempoOnline.java
@@ -1,30 +1,46 @@
 package com.yuhtin.minecraft.lyces.tempoonline;
 
-import com.google.inject.AbstractModule;
-import com.google.inject.Guice;
 import com.google.inject.Injector;
-import com.google.inject.name.Names;
+import com.henryfabio.sqlprovider.common.SQLProvider;
+import com.henryfabio.sqlprovider.mysql.MySQLProvider;
+import com.henryfabio.sqlprovider.mysql.configuration.MySQLConfiguration;
+import com.henryfabio.sqlprovider.sqlite.SQLiteProvider;
+import com.henryfabio.sqlprovider.sqlite.configuration.SQLiteConfiguration;
+import com.yuhtin.minecraft.lyces.tempoonline.guice.PluginModule;
+import lombok.Getter;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.logging.Logger;
+import java.io.File;
 
 public final class TempoOnline extends JavaPlugin {
 
     private Injector injector;
 
+    @Getter private SQLProvider sqlProvider;
+
+    public static TempoOnline getInstance() {
+        return getPlugin(TempoOnline.class);
+    }
+
+    @Override
+    public void onLoad() {
+        saveDefaultConfig();
+    }
+
     @Override
     public void onEnable() {
 
-        this.injector = Guice.createInjector(new AbstractModule() {
-            @Override
-            protected void configure() {
+        configureSqlProvider();
+        this.sqlProvider.connect();
 
-                bind(TempoOnline.class).toInstance(JavaPlugin.getPlugin(TempoOnline.class));
-                bind(Logger.class).annotatedWith(Names.named("main")).toInstance(getLogger());
+        this.getLogger().info("Connection with sql successfully");
 
+        this.injector = PluginModule.from(this).createInjector();
+        this.injector.injectMembers(this);
 
-            }
-        });
+        this.getLogger().info("Guice injection successfully");
 
 
     }
@@ -32,5 +48,34 @@ public final class TempoOnline extends JavaPlugin {
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    private void configureSqlProvider() {
+
+        FileConfiguration configuration = getConfig();
+        if (configuration.getBoolean("connection.mysql.enable")) {
+
+            ConfigurationSection mysqlSection = configuration.getConfigurationSection("connection.mysql");
+
+            sqlProvider = new MySQLProvider(new MySQLConfiguration()
+                    .address(mysqlSection.getString("address"))
+                    .username(mysqlSection.getString("username"))
+                    .password(mysqlSection.getString("password"))
+                    .database(mysqlSection.getString("database"))
+            );
+
+        } else {
+
+            ConfigurationSection sqliteSection = configuration.getConfigurationSection("connection.sqlite");
+
+            sqlProvider = new SQLiteProvider(new SQLiteConfiguration()
+                    .file(new File(
+                            this.getDataFolder(),
+                            sqliteSection.getString("file")
+                    ))
+            );
+
+        }
+
     }
 }
---
+++ src/main/java/com/yuhtin/minecraft/lyces/tempoonline/guice/PluginModule.java
@@ -0,0 +1,44 @@
+package com.yuhtin.minecraft.lyces.tempoonline.guice;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.name.Names;
+import com.henryfabio.sqlprovider.common.SQLProvider;
+import com.yuhtin.minecraft.lyces.tempoonline.TempoOnline;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.bukkit.configuration.Configuration;
+
+import java.util.logging.Logger;
+
+@EqualsAndHashCode(callSuper = false)
+@Data(staticConstructor = "from")
+public class PluginModule extends AbstractModule {
+
+    private final TempoOnline tempoOnline;
+
+    @Override
+    protected void configure() {
+
+        bind(TempoOnline.class)
+                .toInstance(tempoOnline);
+
+        bind(Logger.class)
+                .annotatedWith(Names.named("main"))
+                .toInstance(tempoOnline.getLogger());
+
+        bind(Configuration.class)
+                .annotatedWith(Names.named("main"))
+                .toInstance(tempoOnline.getConfig());
+
+        bind(SQLProvider.class)
+                .toInstance(tempoOnline.getSqlProvider());
+    }
+
+    public Injector createInjector() {
+        return Guice.createInjector(this);
+    }
+
+}
+
