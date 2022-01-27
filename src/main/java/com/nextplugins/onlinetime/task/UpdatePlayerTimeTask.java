4dc32968c3465bd597d5e4cbabb518290465d4cf
MrRafaaeL <rafaelalvessuporte@gmail.com>
2022-01-27 15:01:30 -0300

    Finished

--- src/main/java/com/nextplugins/onlinetime/task/UpdatePlayerTimeTask.java
+++ src/main/java/com/nextplugins/onlinetime/task/UpdatePlayerTimeTask.java
@@ -21,18 +21,18 @@ public class UpdatePlayerTimeTask implements Runnable {
 
             timedPlayer.addTime(System.currentTimeMillis() - timedPlayer.getLastUpdateTime());
             timedPlayer.setLastUpdateTime(System.currentTimeMillis());
-            if (timedPlayer.getTimeInServer() >= 35940000 || timedPlayer.getTimeInServer() <= 36232490) {
+            if (timedPlayer.getTimeInServer() >= 1740001 && timedPlayer.getTimeInServer() <= 2032490) {
+                player.sendTitle("§9§lGG!", "§fVocê está online a mais de 30 minutos.");
+                player.sendMessage("§a§lBOA! §aUma nova recompensa foi liberada no §f/tempo§a.");
+                return;
+            }
+            if (timedPlayer.getTimeInServer() >= 35940000 && timedPlayer.getTimeInServer() <= 36232490) {
                 player.sendTitle("§9§lGG!", "§fVocê está online a mais de 10 horas.");
                 player.sendMessage("§a§lBOA! §aUma nova recompensa foi liberada no §f/tempo§a.");
                 return;
             }
-            if (timedPlayer.getTimeInServer() >= 14340000 || timedPlayer.getTimeInServer() <= 14632490) {
+            if (timedPlayer.getTimeInServer() >= 14340000 && timedPlayer.getTimeInServer() <= 14632490) {
                 player.sendTitle("§9§lGG!", "§fVocê está online a mais de 4 horas.");
-                player.sendMessage("§a§lBOA! §aUma nova recompensa foi liberada no §f/tempo§a.");
-                return;
-            }
-            if (timedPlayer.getTimeInServer() >= 1740001 || timedPlayer.getTimeInServer() <= 2032490) {
-                player.sendTitle("§9§lGG!", "§fVocê está online a mais de 30 minutos.");
                 player.sendMessage("§a§lBOA! §aUma nova recompensa foi liberada no §f/tempo§a.");
                 return;
             }
