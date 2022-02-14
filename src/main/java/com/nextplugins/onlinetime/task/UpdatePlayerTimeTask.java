70e0cb0534a0ad492f88b321c05b3f95ec460e48
MrRafaaeL <rafaelalvessuporte@gmail.com>
2022-02-14 11:45:11 -0300

    Update times

--- src/main/java/com/nextplugins/onlinetime/task/UpdatePlayerTimeTask.java
+++ src/main/java/com/nextplugins/onlinetime/task/UpdatePlayerTimeTask.java
@@ -21,21 +21,6 @@ public class UpdatePlayerTimeTask implements Runnable {
 
             timedPlayer.addTime(System.currentTimeMillis() - timedPlayer.getLastUpdateTime());
             timedPlayer.setLastUpdateTime(System.currentTimeMillis());
-            if (timedPlayer.getTimeInServer() >= 1740001 && timedPlayer.getTimeInServer() <= 2032490) {
-                player.sendTitle("§9§lGG!", "§fVocê está online a mais de 30 minutos.");
-                player.sendMessage("§a§lBOA! §aUma nova recompensa foi liberada no §f/tempo§a.");
-                return;
-            }
-            if (timedPlayer.getTimeInServer() >= 115200000 && timedPlayer.getTimeInServer() <= 118800000) {
-                player.sendTitle("§9§lGG!", "§fVocê está online a mais de 4 horas.");
-                player.sendMessage("§a§lBOA! §aUma nova recompensa foi liberada no §f/tempo§a.");
-                return;
-            }
-            if (timedPlayer.getTimeInServer() >= 921600000 && timedPlayer.getTimeInServer() <= 925200000) {
-                player.sendTitle("§9§lGG!", "§fVocê está online a mais de 10 horas.");
-                player.sendMessage("§a§lBOA! §aUma nova recompensa foi liberada no §f/tempo§a.");
-                return;
-            }
         }
 
     }
