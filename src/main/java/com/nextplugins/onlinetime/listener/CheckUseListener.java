d149debf0e152b596dd04f41902f2a127a07e241
zalyson <goncalvesbgs12@gmail.com>
2022-07-30 15:04:47 -0300

    update

--- src/main/java/com/nextplugins/onlinetime/listener/CheckUseListener.java
+++ src/main/java/com/nextplugins/onlinetime/listener/CheckUseListener.java
@@ -3,7 +3,7 @@ package com.nextplugins.onlinetime.listener;
 import com.nextplugins.onlinetime.configuration.values.MessageValue;
 import com.nextplugins.onlinetime.manager.TimedPlayerManager;
 import com.nextplugins.onlinetime.utils.TimeUtils;
-import de.tr7zw.changeme.nbtapi.NBTItem;
+import de.tr7zw.nbtapi.NBTItem;
 import lombok.AllArgsConstructor;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
@@ -23,20 +23,20 @@ public class CheckUseListener implements Listener {
 
     @EventHandler
     public void onInteract(PlayerInteractEvent event) {
-        ItemStack item = event.getItem();
-        if (item == null || item.getType() == Material.AIR) return;
+	   ItemStack item = event.getItem();
+	   if (item == null || item.getType() == Material.AIR) return;
 
-        NBTItem nbtItem = new NBTItem(item);
-        if (!nbtItem.hasKey("timeInCheck")) return;
+	   NBTItem nbtItem = new NBTItem(item);
+	   if (!nbtItem.hasKey("timeInCheck")) return;
 
-        long timeInCheck = nbtItem.getLong("timeInCheck");
-        this.timedPlayerManager.getByName(event.getPlayer().getName()).addTime(timeInCheck);
+	   long timeInCheck = nbtItem.getLong("timeInCheck");
+	   this.timedPlayerManager.getByName(event.getPlayer().getName()).addTime(timeInCheck);
 
-        event.getPlayer().sendMessage(
-            MessageValue.get(MessageValue::checkActivated)
-                .replace("%time%", TimeUtils.format(timeInCheck))
-        );
+	   event.getPlayer().sendMessage(
+			 MessageValue.get(MessageValue::checkActivated)
+				    .replace("%time%", TimeUtils.format(timeInCheck))
+	   );
 
-        event.getPlayer().setItemInHand(null);
+	   event.getPlayer().setItemInHand(null);
     }
 }
